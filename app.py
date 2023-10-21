13867c03972f516ddbef7ecf83d56e3d7901d2a3
Guillermo Barquero <guillebarq@gmail.com>
2023-10-21 01:48:50 -0400

    Finished basic joke app. Added more joke types and frontend

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import pyjokes 
+import Joking
+import random
   
 app=Flask(__name__) 
   
@@ -10,7 +12,32 @@ def home():
 
 @app.route("/joke")
 def joke():
-    joke=pyjokes.get_joke()  #It only returns one joke. We get random joke each time. 
+    joke=Joking.random_joke()
+    return render_template("joke.html", joke=joke)
+
+@app.route("/animal")
+def animal_joke():
+    joke=Joking.animal_joke()
+    return render_template("joke.html", joke=joke)
+
+@app.route("/dad")
+def dad_joke():
+    joke=Joking.random_dad_joke()
+    return render_template("joke.html", joke=joke)
+
+@app.route("/pun")
+def pun_joke():
+    joke=Joking.Pun()
+    return render_template("joke.html", joke=joke)
+
+@app.route("/dark")
+def dark_joke():
+    joke=Joking.DarkJoke()
+    return render_template("joke.html", joke=joke)
+
+@app.route("/law")
+def law_joke():
+    joke=Joking.Law_Joke()
     return render_template("joke.html", joke=joke)
   
   
